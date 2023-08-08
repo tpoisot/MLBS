187c76e7cc126baad44065bcb67063d0ea0760a2
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-08-08 14:43:02 -0400

    update the data for gradient descent

--- datacleaning/gradientdescent.jl
+++ datacleaning/gradientdescent.jl
@@ -54,3 +54,33 @@ heatmap!(bgmask; colormap = cgrad([:transparent, :white]; alpha = 0.3))
 scatter!(presences; color = :black)
 scatter!(keys(bgpoints); color = :red)
 current_figure()
+
+# Get the data
+replace!(presencelayer, false => nothing)
+pr = keys(presencelayer)
+ab = keys(bgpoints)
+
+lon = vcat([p[1] for p in pr], [p[1] for p in ab])
+lat = vcat([p[2] for p in pr], [p[2] for p in ab])
+pre = vcat([true for p in pr], [false for p in ab])
+
+# Data
+BIO1 = SimpleSDMPredictor(dataprovider; layer = "BIO1", spatial_extent...)
+BIO2 = SimpleSDMPredictor(dataprovider; layer = "BIO2", spatial_extent...)
+BIO3 = SimpleSDMPredictor(dataprovider; layer = "BIO3", spatial_extent...)
+BIO4 = SimpleSDMPredictor(dataprovider; layer = "BIO4", spatial_extent...)
+BIO12 = SimpleSDMPredictor(dataprovider; layer = "BIO12", spatial_extent...)
+
+# data
+bio1 = vcat([BIO1[p] for p in pr], [BIO1[p] for p in ab])
+bio2 = vcat([BIO2[p] for p in pr], [BIO2[p] for p in ab])
+bio3 = vcat([BIO3[p] for p in pr], [BIO3[p] for p in ab])
+bio4 = vcat([BIO4[p] for p in pr], [BIO4[p] for p in ab])
+bio12 = vcat([BIO12[p] for p in pr], [BIO12[p] for p in ab])
+
+using DataFrames
+import CSV
+
+df = DataFrame(latitude=lat, longitude=lon, presence=pre, bio1=bio1, bio2=bio2, bio3=bio3, bio4=bio4, bio12=bio12)
+mkdir("data/gradientdescent")
+CSV.write("data/gradientdescent/climate.csv", df)
