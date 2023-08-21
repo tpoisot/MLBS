13579d6587bf1cf1d660e648243df89d46896ac9
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-08-21 10:09:42 -0400

    update code

--- lib/nbc.jl
+++ lib/nbc.jl
@@ -1,6 +1,9 @@
-function naivebayes(y, X; presence=0.5, variables=nothing)
+using Distributions
+using Statistics
+
+function naivebayes(y::Vector{Bool}, X::Matrix{T}; presence=0.5, variables=nothing) where {T <: Number}
     if isnothing(variables)
-        variables = collect(axes(X, 1))
+        variables = collect(axes(X, 2))
     end
     μ = mapslices(mean, X, dims=1)
     σ = mapslices(std, X, dims=1)
@@ -8,7 +11,7 @@ function naivebayes(y, X; presence=0.5, variables=nothing)
     Xneg = (X[findall(.!y),:] .- μ) ./ σ
     pred_pos = mapslices(x -> Normal(mean(x), std(x)), Xpos, dims=1)
     pred_neg = mapslices(x -> Normal(mean(x), std(x)), Xneg, dims=1)
-    function inner_predictor(v)
+    function inner_predictor(v::Vector{TN}) where { TN <: Number }
         nv = (v' .- μ) ./ σ
         is_pos = prod(pdf.(pred_pos, nv)[variables])
         is_neg = prod(pdf.(pred_neg, nv)[variables])
---
+++ lib/pkg.jl
@@ -0,0 +1,7 @@
+using DataFrames
+import CSV
+
+using Statistics
+
+using CairoMakie
+CairoMakie.activate!(; px_per_unit = 2)
