8a9066d9894fc33f2a9b128b7c6e9234e8b2998e
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-08-26 17:01:38 -0400

    clustering data update

--- datacleaning/kmeans.jl
+++ datacleaning/kmeans.jl
@@ -13,33 +13,36 @@ function readLS9band(files, pattern; kwargs...)
     return SpeciesDistributionToolkit._read_geotiff(band_file, SimpleSDMResponse; kwargs...)
 end
 
-#b5 = readLS9band(img_files, "B5")
-#heatmap(b5)
+b5 = readLS9band(img_files, "B5")
+heatmap(b5)
+bbox = (left=8.95, right=9.3, bottom=41.35, top=41.65)
+clp = clip(b5; bbox...)
+@info size(clp)
+heatmap(clp, colormap=:vik, aspect=DataAspect())
 
-bbox = (left=-72.8, right=-72.6, bottom=19.4, top=19.5)
-#b5 = readLS9band(img_files, "B2"; bbox...)
-#heatmap(b5)
-
-nir = readLS9band(img_files, "B5"; bbox...)
-swir = readLS9band(img_files, "B6"; bbox...)
 blue = readLS9band(img_files, "B2"; bbox...)
 green = readLS9band(img_files, "B3"; bbox...)
 red = readLS9band(img_files, "B4"; bbox...)
+nir = readLS9band(img_files, "B5"; bbox...)
+swir1 = readLS9band(img_files, "B6"; bbox...)
+swir2 = readLS9band(img_files, "B7"; bbox...)
 
 T = SimpleSDMLayers._inner_type(red)
 
-for layer in [red, green, blue, nir, swir]
+for layer in [red, green, blue, nir, swir1, swir2]
     replace!(layer, nothing => zero(T))
 end
 
 R = convert(Matrix{Float16}, grid(red))
 G = convert(Matrix{Float16}, grid(green))
 B = convert(Matrix{Float16}, grid(blue))
-S = convert(Matrix{Float16}, grid(swir))
+S1 = convert(Matrix{Float16}, grid(swir1))
+S2 = convert(Matrix{Float16}, grid(swir2))
 N = convert(Matrix{Float16}, grid(nir))
 
 writedlm("data/kmeans/cooked/red.dat", R)
 writedlm("data/kmeans/cooked/green.dat", G)
 writedlm("data/kmeans/cooked/blue.dat", B)
 writedlm("data/kmeans/cooked/nir.dat", N)
-writedlm("data/kmeans/cooked/swir.dat", S)
+writedlm("data/kmeans/cooked/swir1.dat", S1)
+writedlm("data/kmeans/cooked/swir2.dat", S2)
--- lib/colorcube.jl
+++ lib/colorcube.jl
@@ -1,10 +1,10 @@
 using Images
 
 function colorcube(R, G, B; scaler=2.75e-05)
-    cube = zeros(eltype(B), (3, size(B)...))
-    cube[1,:,:] .= R
-    cube[2,:,:] .= G
-    cube[3,:,:] .= B
+    cube = zeros(eltype(B), (3, size(permutedims(B))...))
+    cube[1,:,:] .= permutedims(R)
+    cube[2,:,:] .= permutedims(G)
+    cube[3,:,:] .= permutedims(B)
 
     tfile = tempname()*".png"
     save(tfile, map(clamp01nan, colorview(RGB, cube.*scaler)))
