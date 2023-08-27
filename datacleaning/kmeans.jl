0749ec39695039596ba82765f2cbb9ff548e1912
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-08-27 16:06:50 -0400

    data image update

--- datacleaning/kmeans.jl
+++ datacleaning/kmeans.jl
@@ -4,28 +4,36 @@ using SpeciesDistributionToolkit
 using DelimitedFiles
 using CairoMakie
 
-img_path = "data/kmeans/raw"
-img_files = readdir(img_path; join=true)
-filter!(contains(".TIF"), img_files)
+img_path = "data/kmeans/raw/LC08_L2SP_017024_20230602_20230607_02_T1/"
+all_files = readdir(img_path; join=true)
+bands = filter(contains(r"SR_B\d.TIF"), all_files)
 
 function readLS9band(files, pattern; kwargs...)
     band_file = only(filter(contains("$(pattern).TIF"), files))
     return SpeciesDistributionToolkit._read_geotiff(band_file, SimpleSDMResponse; kwargs...)
 end
 
-b5 = readLS9band(img_files, "B5")
-heatmap(b5)
-bbox = (left=9.05, right=9.27, bottom=41.35, top=41.5)
-clp = clip(b5; bbox...)
-@info size(clp)
-heatmap(clp, colormap=:vik, aspect=DataAspect())
+testband = readLS9band(img_files, "B6")
+heatmap(testband, colormap=:Spectral)
 
-blue = readLS9band(img_files, "B2"; bbox...)
-green = readLS9band(img_files, "B3"; bbox...)
-red = readLS9band(img_files, "B4"; bbox...)
-nir = readLS9band(img_files, "B5"; bbox...)
-swir1 = readLS9band(img_files, "B6"; bbox...)
-swir2 = readLS9band(img_files, "B7"; bbox...)
+bbox = (left=-76.3, right=-76.15, bottom=51.1, top=51.25)
+clp = clip(testband; bbox...)
+@info size(clp)
+heatmap(clp, colormap=:Spectral)
+
+function get_and_correct_band(img_files, bid, bbox)
+    raw_bd = readLS9band(img_files, "B$(bid)"; bbox...)
+    scaled =  raw_bd .* 0.0000275 .- 0.2
+    normed = (scaled .+ 0.199972)./(1.602213 + 0.199972)
+    return normed
+end
+
+blue = get_and_correct_band(img_files, 2, bbox)
+green = get_and_correct_band(img_files, 3, bbox)
+red = get_and_correct_band(img_files, 4, bbox)
+nir = get_and_correct_band(img_files, 5, bbox)
+swir1 = get_and_correct_band(img_files, 6, bbox)
+swir2 = get_and_correct_band(img_files, 7, bbox)
 
 T = SimpleSDMLayers._inner_type(red)
 
@@ -46,3 +54,11 @@ writedlm("data/kmeans/cooked/blue.dat", B)
 writedlm("data/kmeans/cooked/nir.dat", N)
 writedlm("data/kmeans/cooked/swir1.dat", S1)
 writedlm("data/kmeans/cooked/swir2.dat", S2)
+
+ndvi = @. (N - R) / (N + R)
+ndwi = @. (G - N) / (G + N)
+ndmi = @. (N - S1) / (N + S1)
+
+fig, ax, hm = heatmap(permutedims(ndwi), colormap=:Spectral, colorrange=(-0.1, 0.1))
+Colorbar(fig[:, end+1], hm)
+fig
--- lib/colorcube.jl
+++ lib/colorcube.jl
@@ -1,12 +1,22 @@
 using Images
 
-function colorcube(R, G, B; scaler=2.75e-05)
+function rscale(Y; q=(0.001, 0.999))
+    m, M = quantile(vec(Y), q)
+    return clamp01.((Y .- m)./(M - m))
+end
+
+function colorcube(R, G, B; natural=false)
     cube = zeros(eltype(B), (3, size(permutedims(B))...))
-    cube[1,:,:] .= permutedims(R)
-    cube[2,:,:] .= permutedims(G)
-    cube[3,:,:] .= permutedims(B)
+
+    qR = rscale(R)
+    qG = rscale(G)
+    qB = rscale(B)
+
+    cube[1,:,:] .= permutedims(qR)
+    cube[2,:,:] .= permutedims(qG)
+    cube[3,:,:] .= permutedims(qB)
 
     tfile = tempname()*".png"
-    save(tfile, map(clamp01nan, colorview(RGB, cube.*scaler)))
+    save(tfile, map(clamp01nan, colorview(RGB, cube)))
     return tfile
 end
