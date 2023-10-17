2b1f34f0faba3c0e1707dba59d76d06427a20fec
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-10-17 09:30:31 -0400

    add map for the model uncertainty

--- lib/crossvalidate.jl
+++ lib/crossvalidate.jl
@@ -11,3 +11,11 @@ function crossvalidate(model, y, X, folds, args...; kwargs...)
     end
     return Cv, Ct
 end
+
+function iqr(x)
+    if all(isnan.(x))
+        return 0.0
+    else
+        return first(diff(quantile(filter(!isnan, x), [0.25, 0.75])))
+    end
+end
