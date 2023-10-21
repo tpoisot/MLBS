7390bbadf45b2798d4c7a280c244ad1dcabd84bc
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-10-21 14:25:58 -0400

    add coinflip CM

--- lib/mocks.jl
+++ lib/mocks.jl
@@ -7,3 +7,13 @@ function noskill(labels::Vector{Bool})
     fn = round(Int, n*((1-p)*p))
     ConfusionMatrix(tp, tn, fp, fn)
 end
+
+function coinflip(labels::Vector{Bool})
+    n = length(labels)
+    p = mean(labels)
+    tp = round(Int, n/2*p)
+    tn = round(Int, n/2*p)
+    fp = round(Int, n/2*(1-p))
+    fn = round(Int, n/2*(1-p))
+    ConfusionMatrix(tp, tn, fp, fn)
+end
