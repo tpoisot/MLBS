a2fe1d88b5289e0c24f6176ba4c8b575c1066b9c
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-12-08 15:29:44 -0500

    fix vif

--- code/minisdm/vif.jl
+++ code/minisdm/vif.jl
@@ -3,18 +3,15 @@ using GLM
 vif(m) = 1 / (1 - r²(m))
 
 function stepwisevif(X::Matrix{T}, threshold) where {T <: Number}
-    return stepwisevif(X, collect(1:size(X, 2)), threshold)
+    return stepwisevif(X, collect(1:size(X, 1)), threshold)
 end
 
 function stepwisevif(X::Matrix{T}, v, threshold) where {T <: Number}
-    X = (X .- mean(X; dims = 1)) ./ std(X; dims = 1)
+    X = (X .- mean(X; dims = 2)) ./ std(X; dims = 2)
     vifs = zeros(Float64, length(v))
-    for i in eachindex(v)
-        begin
-            # GLM gives warning with matrices
-            model = lm(X[:, setdiff(eachindex(v), i)], X[:, i])
-            vifs[i] = vif(model)
-        end
+    for i in eachindex(v)        
+        model = lm(X[setdiff(eachindex(v), i), :]', X[i, :])
+        vifs[i] = vif(model)
     end
     all(vifs .<= threshold) && return v
     drop = last(findmax(vifs))
