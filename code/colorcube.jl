8b624c3036043f27003417363b7f916aa6321cd7
Timothée Poisot <timothee.poisot@umontreal.ca>
2023-12-09 12:05:03 -0500

    clustering in progress

--- code/colorcube.jl
+++ code/colorcube.jl
@@ -1,24 +1,23 @@
+using Images
+
 function rscale(Y; q = (0.01, 0.99))
     m, M = quantile(vec(Y), q)
     return Images.clamp01.((Y .- m) ./ (M - m))
 end
 
-function colorcube(R, G, B; natural = false)
-    cube = zeros(eltype(B), (3, size(permutedims(B))...))
+function colorcube(R, G, B)
+    r = convert(Matrix{Float32}, R.grid)
+    g = convert(Matrix{Float32}, G.grid)
+    b = convert(Matrix{Float32}, B.grid)
+    cube = zeros(Float32, (3, size(permutedims(b))...))
 
-    if natural
-        qR = Images.clamp01.((R .- 0.10) ./ 0.18)
-        qG = Images.clamp01.((G .- 0.10) ./ 0.13)
-        qB = Images.clamp01.((B .- 0.10) ./ 0.15)
-    else
-        qR = rscale(R)
-        qG = rscale(G)
-        qB = rscale(B)
-    end
+    qR = rscale(r)
+    qG = rscale(g)
+    qB = rscale(b)
 
-    cube[1, :, :] .= permutedims(qR)
-    cube[2, :, :] .= permutedims(qG)
-    cube[3, :, :] .= permutedims(qB)
+    cube[1, :, :] .= reverse(rotl90(qR); dims=1)
+    cube[2, :, :] .= reverse(rotl90(qG); dims=1)
+    cube[3, :, :] .= reverse(rotl90(qB); dims=1)
 
     tfile = tempname() * ".png"
     save(tfile, map(Images.clamp01nan, Images.colorview(Images.RGB, cube)))
