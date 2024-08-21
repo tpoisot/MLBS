f5d89ba3995346e0a6a8b3fe7fe9085d3062b44b
tpoisot <timothee.poisot@umontreal.ca>
2024-08-21 15:27:57 -0400

    fix shapley

--- code/minisdm/shapley.jl
+++ code/minisdm/shapley.jl
@@ -1,38 +1,49 @@
-import Random
-
-function shapleyvalues(model, X, i::T, j::T; kwargs...) where {T <: Int}
-    x = X[i,:]
-    return shapleyvalues(model, X, x, j; kwargs...)
-end
-
-function shapleyvalues(model, X::Matrix{T1}, x::Vector{T2}, j; M=200) where {T1 <: Number, T2 <: Number}
-
-    ϕ = zeros(Float64, M)
-    b1 = copy(x)
-    b2 = copy(x)
-
-    for m in axes(ϕ, 1)
-        O = Random.shuffle(axes(X, 2))
-        w = X[sample(axes(X, 1)),:]
-
-        i = only(indexin(j, O))
-        for (idx,pos) in enumerate(O)
-            if idx < i
-                b1[pos] = x[pos]
-                b2[pos] = x[pos]
-            end
-            if idx > i
-                b1[pos] = w[pos]
-                b2[pos] = w[pos]
-            end
-            if idx == i
-                b1[pos] = x[pos]
-                b2[pos] = w[pos]
+function mcsample(X, Z, i, j, n)
+    # Initial sample matrix
+    ξ = zeros(eltype(X), size(X, 1), n)
+    for instance in axes(ξ, 2)
+        ξ[:,instance] = X[:,i]
+    end
+    # Observations for boostrap
+    p = rand(axes(Z, 2), n)
+    # And now we shuffle
+    shuffling = rand(Bool, size(ξ))
+    for ob in axes(shuffling, 2)
+        for va in axes(shuffling, 1)
+            if va != j
+                if shuffling[va, ob]
+                    ξ[va, ob] = Z[va, p[ob]]
+                end
             end
         end
-        
-        ϕ[m] = model(b1) - model(b2)
     end
+    ζ = copy(ξ)
+    ζ[j,:] .= X[j, p]
+    return ξ, ζ
+end
 
-    return sum(ϕ)/M
+mcsample(X, i, j, n) = mcsample(X, X, i, j, n)
+
+function shap_one_point(f, X, Z, i, j, n)
+    ξ, ζ = mcsample(X, Z, i, j, n)
+    ϕ = f(ξ) - f(ζ)
+    return sum(ϕ)/length(ϕ)
+end
+
+function shap_list_points(f, X, Z, i, j, n)
+    p0 = shap_one_point(f, X, Z, first(i), j, n)
+    vals = Vector{typeof(p0)}(undef, length(i))
+    vals[begin] = p0
+    chunks = ceil.(Int, LinRange(1, length(i), Threads.nthreads()+1))
+    Threads.@threads for thr in 1:Threads.nthreads()
+        bg, nd = chunks[thr]+1, chunks[thr+1]
+        for smpl in bg:nd
+            vals[smpl] = shap_one_point(f, X, Z, i[smpl], j, n)
+        end
+    end
+    return vals
+end
+
+function shap_all_points(f, X, Z, j, n)
+    return shap_list_points(f, X, Z, axes(X, 1), j, n)
 end
