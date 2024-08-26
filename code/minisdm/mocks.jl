f1deee70285f27926bc22d450084307bd6281d10
tpoisot <timothee.poisot@umontreal.ca>
2024-08-25 22:05:10 -0400

    update text

--- code/minisdm/mocks.jl
+++ code/minisdm/mocks.jl
@@ -16,3 +16,21 @@ function coinflip(labels::Vector{Bool})
     fn = 1 / 2 * (1 - p)
     return ConfusionMatrix(tp, tn, fp, fn)
 end
+
+function constantpositive(labels::Vector{Bool})
+    p = mean(labels)
+    tp = p
+    tn = 0.
+    fp = (1 - p)
+    fn = 0.
+    return ConfusionMatrix(tp, tn, fp, fn)
+end
+
+function constantnegative(labels::Vector{Bool})
+    p = mean(labels)
+    tp = 0.
+    tn = (1 - p)
+    fp = 0.
+    fn = p
+    return ConfusionMatrix(tp, tn, fp, fn)
+end
